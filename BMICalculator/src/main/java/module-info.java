154d30cba02783fed90af409f1acf6062ff3a5a0
77894100+Umidbek25@users.noreply.github.com <umedjonulmasov1619@gmail.com>
2022-12-21 00:18:02 +0600

    BMI added

---
+++ BMICalculator/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.bmicalculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.bmicalculator to javafx.fxml;
+    exports com.example.bmicalculator;
+}
